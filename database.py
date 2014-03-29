ccc74f184f372556722f0ca7fb8153e28fce2466
Jonathan Marmor <jmarmor@gmail.com>
2014-03-29 09:14:05 -0400

    Not using mongo or redis, at least for now.

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from pymongo import MongoClient
-import redis
+# from pymongo import MongoClient
+# import redis
 
 
 class Database(object):
     def __init__(self):
-        self.mongo_conn = MongoClient('localhost', 27017)
-        self.mongo = self.mongo_conn.animalplay
-        self.r = redis.Redis()
+        # self.mongo_conn = MongoClient('localhost', 27017)
+        # self.mongo = self.mongo_conn.animalplay
+        # self.r = redis.Redis()
+        pass
