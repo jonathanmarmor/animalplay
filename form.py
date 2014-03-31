003ac060aeb92e728bc25ed6ac4db48c69215d2f
Jonathan Marmor <jmarmor@gmail.com>
2014-03-30 21:38:46 -0400

    Soloist and accompanist entrances and exits. Broke dynamics for those parts.

--- form.py
+++ form.py
@@ -96,12 +96,20 @@ class Form(object):
         self.group_sections()
         self.set_staves()
 
+        self.adjust_soloist_entrances()
+
+        movement = None
+        for b in self.bars:
+            if movement != b['movement_name']:
+                movement = b['movement_name']
+                print movement
+            print b['soloist'], b['accompanists']
+
         self.temp_fill_with_rests()
 
         self.make_drones()
         self.make_harmonic_rhythm()
         self.choose_harmonies()
-
         self.make_bassline()
 
         self.add_rehearsal_marks()
@@ -128,19 +136,22 @@ class Form(object):
                             bar_index=bar_index,
                             soloist=Conf.soloist[movement_number],
                             accompanists=Conf.accompanists[movement_number] if volume.startswith('l') else (),
-                            dynamics = Conf.dynamics[movement_number][volume[0]],
+                            dynamics=Conf.dynamics[movement_number][volume[0]],
                         )
                         bar['dynamics']['drone'] = Conf.dynamics[movement_number]['drone']
                         self.bars.append(bar)
                         bar_index += 1
 
-    def group_section(self, attr):
-        return [list(group) for key, group in groupby(self.bars, lambda x: x[attr])]
+    def group_section(self, bars, attr):
+        """Group a list of bar configs by a config attribute"""
+        return [list(group) for key, group in groupby(bars, lambda x: x[attr])]
 
     def group_sections(self):
-        self.movement_sections = self.group_section('movement_number')
-        self.drone_sections = self.group_section('drone')
-        self.volume_sections = self.group_section('volume')
+        self.movement_sections = self.group_section(self.bars, 'movement_number')
+        self.drone_sections = self.group_section(self.bars, 'drone')
+        self.volume_sections = self.group_section(self.bars, 'volume')
+
+        self.movement_volume_sections = [self.group_section(m, 'volume') for m in self.movement_sections]
 
     def set_staves(self):
         self.staves = []
@@ -181,17 +192,21 @@ class Form(object):
             first_bar_conf = section[0]
             bar_index = first_bar_conf['bar_index']
 
-            soloist = self.score[first_bar_conf['soloist']]
-            soloist_first_note = soloist[bar_index][0]
-            add_dynamic(soloist_first_note, first_bar_conf['dynamics']['soloist'])
-
-            for acc_name in first_bar_conf['accompanists']:
-                acc = self.score[acc_name]
-                first_note = acc[bar_index][0]
-                add_dynamic(first_note, first_bar_conf['dynamics']['accompanists'])
-
             piano_first_note = piano[bar_index][0]
             add_dynamic(piano_first_note, first_bar_conf['dynamics']['piano'])
+
+        # for section in self.volume_sections:
+        #     first_bar_conf = section[0]
+        #     bar_index = first_bar_conf['bar_index']
+
+        #     soloist = self.score[first_bar_conf['soloist']]
+        #     soloist_first_note = soloist[bar_index][0]
+        #     add_dynamic(soloist_first_note, first_bar_conf['dynamics']['soloist'])
+
+        #     for acc_name in first_bar_conf['accompanists']:
+        #         acc = self.score[acc_name]
+        #         first_note = acc[bar_index][0]
+        #         add_dynamic(first_note, first_bar_conf['dynamics']['accompanists'])
 
         synth = self.score['Synthesizer']
         # TODO use the section where the drone is actually sounding, not the square section
@@ -260,8 +275,53 @@ class Form(object):
                 # bar_config['harmonies_not_covered'][i].remove(abs(p % 12))
 
             bar_config['piano_lower_pitches'] = pitches
-
             bar = get_bar(harmonic_rhythm, pitches)
-
             piano_lower.append(bar)
 
+    def adjust_soloist_entrances(self):
+        movements = self.movement_sections
+        for i, movement in enumerate(movements):
+            next_i = (i + 1) % len(movements)
+            next_soloist = movements[next_i][0]['soloist']
+
+            for bar in movements[next_i]:
+                if bar['accompanists'] != ():
+                    next_accompanists = bar['accompanists']
+                    break
+
+            last_section = self.movement_volume_sections[i][-1]
+            for bar_config in last_section:
+                bar_config['accompanists'] = next_accompanists
+
+
+            # Pick soloist exit and new soloist entrance
+
+            # No one should ever write code like this
+            last_sections = self.movement_volume_sections[i][-3:]
+            last_sections[0] = last_sections[0][len(last_sections[0]) / 2:]
+            last_sections[-1] = last_sections[-1][:len(last_sections[-1]) / 2]
+
+            lengths = [len(section) for section in last_sections]
+
+            flat = []
+            for section in last_sections:
+                for bar in section:
+                    flat.append(bar)
+
+            exit_bar_index = random.choice(range(sum(lengths[:2])))
+
+            for bar in flat[exit_bar_index:]:
+                bar['soloist'] = None
+
+            for bar in last_section[len(last_section) / 2:]:
+                bar['soloist'] = None
+
+
+            possible_entrance_bars = flat[exit_bar_index + 1:]
+            entrance_bar_index = random.choice(range(len(possible_entrance_bars)))
+            new_solist_bars = possible_entrance_bars[entrance_bar_index:]
+            for bar in new_solist_bars:
+                bar['soloist'] = next_soloist
+
+            for bar in last_section[len(last_section) / 2:]:
+                bar['soloist'] = next_soloist
